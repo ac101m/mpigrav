99adcc7fea23b8d1f980be30c8240e95ddbd1c41
ac101m <mofty101@gmail.com>
2019-03-10 21:43:59 +0000

    Add option parser

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main(void) {
-  std::cout << "I am the compute engine\n";
+#include <optparse.hpp>
+
+#include "mpi.h"
+
+
+void AddOptions(OptionParser& opt) {
+  opt.Add(Option("gravitation", 'g', ARG_TYPE_FLOAT,
+                 "Gravitational constant for the simulation",
+                 {"6.67408E-11"}));
+}
+
+
+int main(int argc, char **argv) {
+  OptionParser opt(argc, argv, "mpigrav compute engine");
+  AddOptions(opt);
+
   return 0;
 }
--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <unistd.h>
-#include <GLT/Window.hpp>
 
-int main(void) {
-  std::cout << "I am the viewer app\n";
-  GLT::Window window(640, 480, "viewer");
-  usleep(1000000);
+#include <GLT/Window.hpp>
+#include <optparse.hpp>
+
+
+void AddOptions(OptionParser& opt) {
+  opt.Add(Option("host", 'h', ARG_TYPE_STRING,
+                 "IP address of host to connect to.",
+                 {"127.0.0.1"}));
+}
+
+
+int main(int argc, char **argv) {
+  OptionParser opt(argc, argv, "mpigrav viewer application");
+  AddOptions(opt);
   return 0;
 }
