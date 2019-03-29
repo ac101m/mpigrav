b81584e38c13ee541789dbfe63fccbaf798dd6e3
ac101m <mofty101@gmail.com>
2019-03-29 20:55:45 +0000

    Remove defunct str method from body class

--- include/Body.hpp
+++ include/Body.hpp
@@ -12,7 +12,7 @@ class Body {
   public:
     Vec3 pos;      // Position, meters
     Vec3 v;        // Velocity, meters per second
-    fp_t m;              // Mass, kilograms
+    fp_t m;        // Mass, kilograms
 
   public:
     Body(void) : m(1) {}
@@ -21,13 +21,6 @@ class Body {
     // Applies veolocity with given timestep
     inline void Update(fp_t const dt) {this->pos = this->pos + (v * dt);}
     inline Vec3 Force(Body const& other);
-
-    std::string Str(void) {
-      std::stringstream ss;
-      ss << "Pos: " << this->pos.Str();
-      ss << "\tv: " << sqrt(v.x*v.x + v.y*v.y + v.z*v.z) << "m/s";
-      return ss.str();
-    }
 };
 
 
