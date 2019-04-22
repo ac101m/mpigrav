f8ddd6f8ae5a98ed27443a8efdd37ec865c46289
ac101m <mofty101@gmail.com>
2019-04-22 17:02:41 +0100

    Move vec3 arithmetic operators into header

--- include/util/Vec3.hpp
+++ include/util/Vec3.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <cmath>
 
 #include "Master.hpp"
 
@@ -25,21 +26,74 @@ class Vec3 {
 };
 
 
-// Math operators
-Vec3 operator+(Vec3 lhs, Vec3 const& rhs);
-Vec3 operator-(Vec3 lhs, Vec3 const& rhs);
-Vec3 operator*(Vec3 lhs, Vec3 const& rhs);
-Vec3 operator*(Vec3 lhs, float const& rhs);
-Vec3 operator/(Vec3 lhs, Vec3 const& rhs);
-Vec3 operator/(Vec3 lhs, float const& rhs);
+//====[ARITHMETIC]===========================================================//
 
-// Comparison
-bool operator!=(Vec3 const& lhs, Vec3 const& rhs);
-bool operator==(Vec3 const& lhs, Vec3 const& rhs);
+// Addition
+inline Vec3 operator+(Vec3 lhs, Vec3 const& rhs) {
+  lhs.x += rhs.x; lhs.y += rhs.y; lhs.z += rhs.z;
+  return lhs;
+}
 
-// General
-Vec3 Normalize(Vec3 lhs);
-float Magnitude(Vec3 const& lhs);
+// Subtraction
+inline Vec3 operator-(Vec3 lhs, Vec3 const& rhs) {
+  lhs.x -= rhs.x; lhs.y -= rhs.y; lhs.z -= rhs.z;
+  return lhs;
+}
+
+// Multiplication - vector
+inline Vec3 operator*(Vec3 lhs, Vec3 const& rhs) {
+  lhs.x *= rhs.x; lhs.y *= rhs.y; lhs.z *= rhs.z;
+  return lhs;
+}
+
+// Multiplication - single
+inline Vec3 operator*(Vec3 lhs, float const& rhs) {
+  lhs.x *= rhs; lhs.y *= rhs; lhs.z *= rhs;
+  return lhs;
+}
+
+// Division - vector
+inline Vec3 operator/(Vec3 lhs, Vec3 const& rhs) {
+  lhs.x /= rhs.x; lhs.y /= rhs.y; lhs.z /= rhs.z;
+  return lhs;
+}
+
+// Division - single
+inline Vec3 operator/(Vec3 lhs, float const& rhs) {
+  lhs.x /= rhs; lhs.y /= rhs; lhs.z /= rhs;
+  return lhs;
+}
+
+
+//====[COMPARISON]===========================================================//
+
+// Not equal
+inline bool operator!=(Vec3 const& lhs, Vec3 const& rhs) {
+  return (lhs.x != rhs.x) || (lhs.y != rhs.y) || (lhs.z != rhs.z);
+}
+
+// Equal
+inline bool operator==(Vec3 const& lhs, Vec3 const& rhs) {
+  return !(lhs != rhs);
+}
+
+//====[GENERAL]==============================================================//
+
+// Normalize
+inline Vec3 Normalize(Vec3 lhs) {
+  float length;
+  length = sqrt(lhs.x*lhs.x + lhs.y*lhs.y + lhs.z*lhs.z);
+  lhs.x /= length;
+  lhs.y /= length;
+  lhs.z /= length;
+  return lhs;
+}
+
+
+// Get magnitude of vector
+inline float Magnitude(Vec3 const& lhs) {
+  return sqrt(lhs.x*lhs.x + lhs.y*lhs.y + lhs.z*lhs.z);
+}
 
 
 #endif // _MPIGRAV_VEC3_INCLUDED
