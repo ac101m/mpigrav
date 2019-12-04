b4ade7c203b3c4d62ed92ee7bdc8dcd28608c6fe
ac101m <mofty101@gmail.com>
2019-12-04 23:08:43 +0000

    Revert to cl.hpp

--- include/compute/Universe.hpp
+++ include/compute/Universe.hpp
@@ -7,9 +7,8 @@
 
 
 // External
-#define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 200
-#include <CL/cl2.hpp>
+#define __CL_ENABLE_EXCEPTIONS
+#include <CL/cl.hpp>
 
 
 // Internal
